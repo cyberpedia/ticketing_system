208cc1b12f4c62318f66d737a6e1334c0fe1c6d3
cyberpedia <cyberpedia.ops@gmail.com>
2025-04-14 13:02:18 +0800

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Optional, Length, EqualTo
+from flask_wtf.file import FileField, FileAllowed
 
 class ProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
@@ -8,3 +9,9 @@ class ProfileForm(FlaskForm):
     password = PasswordField('New Password', validators=[Optional(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[Optional(), EqualTo('password')])
     submit = SubmitField('Update Profile')
+
+class TicketForm(FlaskForm):
+    subject = StringField('Subject', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    attachment = FileField('Attach File', validators=[FileAllowed(['jpg', 'jpeg', 'png', 'pdf', 'zip'], 'Invalid file type')])
+    submit = SubmitField('Submit Ticket')
