2933dd903d9ec215d1fe3f8afc74fdc26cdb968c
cyberpedia <cyberpedia.ops@gmail.com>
2025-04-14 13:04:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///ticketing.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'app/static/uploads'
+    //UPLOAD_FOLDER = 'app/static/uploads'
+    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
+    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'pdf', 'zip'}
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
 
 class DevConfig(Config):
