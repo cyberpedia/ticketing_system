daa62538a80102d13a86bf249c8dd994764da81e
cyberpedia <cyberpedia.ops@gmail.com>
2025-04-14 13:20:30 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 migrate = Migrate()
+
+socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +18,7 @@ def create_app():
     app.config['DEBUG'] = True
 
     db.init_app(app)
+    socketio.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
 
