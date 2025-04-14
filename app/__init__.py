51870926625533872827ed3b927769a64d679372
cyberpedia <cyberpedia.ops@gmail.com>
2025-04-14 13:28:16 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
+import logging
+from logging.handlers import RotatingFileHandler
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -27,5 +29,19 @@ def create_app():
 
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    )
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+    
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('App startup')
 
     return app
