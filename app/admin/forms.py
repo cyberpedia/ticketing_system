25e40a9d82d4b9a443c7c6bc21292018650337ef
cyberpedia <cyberpedia.ops@gmail.com>
2025-04-16 15:16:19 +0800

    Delete app directory

--- app/admin/forms.py
+++
@@ -1,7 +0,0 @@
-class AdminTicketForm(FlaskForm):
-    subject = StringField('Subject', validators=[DataRequired()])
-    description = StringField('Description')
-    status = SelectField('Status', choices=[('open', 'Open'), ('in_progress', 'In Progress'), ('closed', 'Closed')])
-    priority = SelectField('Priority', choices=[('Low', 'Low'), ('Normal', 'Normal'), ('High', 'High')])
-    assigned_to = SelectField('Assign To', coerce=int)
-    submit = SubmitField('Update')
